be9eaa942420d9a0e901b4c30c5b2c4a4aa0aabe
k3eenz <osipenkomd@mer.ci.nsu.ru>
2026-03-30 11:40:35 +0700

    fix circular

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,20 @@
-"""Module for application routes."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app.routes import main
 
 db = SQLAlchemy()
 
 def create_app():
-    """Creates Flask"""
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'developer-geo-app'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///geo_quiz.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'developer-geo-app'
 
     db.init_app(app)
 
+    from app.routes import main
     app.register_blueprint(main)
 
     with app.app_context():
+        from . import models
         db.create_all()
 
     return app
